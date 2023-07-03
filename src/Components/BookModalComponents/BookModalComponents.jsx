ff33e28d0498e140898833e4faf45a1280450059
jihad-hossain1 <jihadkhan934@gmail.com>
2023-07-03 14:15:32 +0600

    booking information update on modal

---
+++ src/Components/BookModalComponents/BookModalComponents.jsx
@@ -0,0 +1,31 @@
+import { Modal } from "antd";
+import React from "react";
+
+const BookModalComponents = ({
+  handleCancel,
+  handleOk,
+  isModalOpen,
+  items,
+}) => {
+  console.log(items);
+  return (
+    <>
+      <Modal
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        cancelButtonProps={{ style: { display: "none" } }}
+        okButtonProps={{ style: { display: "none" } }}
+      >
+        <div className="flex flex-col justify-center items-center space-y-2">
+          <h4 className="text-xl font-semibold">{items?.name}</h4>
+          <p>Schedule time: {items?.schedule}</p>
+          <p>Pay Time: {items?.date}</p>
+          <p>Price: ${items?.price}</p>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default BookModalComponents;
--- src/Pages/Dashboard/MyAppointment/MyBooking.jsx
+++ src/Pages/Dashboard/MyAppointment/MyBooking.jsx
@@ -1,14 +1,52 @@
 import React from "react";
 import useBookInfo from "../../../hook/useBookInfo";
+import { useState } from "react";
+import BookModalComponents from "../../../Components/BookModalComponents/BookModalComponents";
 
 const MyBooking = () => {
   const [bookInfo] = useBookInfo();
   console.log(bookInfo);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [items, setItems] = useState();
+  const showModal = (item) => {
+    setItems(item);
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div>
+    <div className="px-4">
       <h4 className="text-4xl text-center font-thin text-neutral-700 py-8">
         My Total Booking: {bookInfo?.length}
       </h4>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {bookInfo.map((item) => (
+          <div
+            onClick={() => showModal(item)}
+            key={item._id}
+            className="border border-neutral-200 p-3 rounded shadow hover:shadow-md cursor-pointer"
+          >
+            <h4 className="text-gray-700 font-semibold text-xl">
+              {item?.name}
+            </h4>
+            <p>
+              Transaction Id:{" "}
+              <span className="text-green-600">{item?.transactionId}</span>
+            </p>
+          </div>
+        ))}
+      </div>
+      <BookModalComponents
+        isModalOpen={isModalOpen}
+        handleOk={handleOk}
+        handleCancel={handleCancel}
+        items={items}
+      ></BookModalComponents>
     </div>
   );
 };
